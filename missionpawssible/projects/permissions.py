c2fc8ffe35d0e17b342d7df6e50e477b3a123e69
Ju Lacerda <julacerdarod@gmail.com>
2021-09-21 18:27:21 +1000

    First commitment

---
+++ missionpawssible/projects/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # only allow write access if the object is owned by the logged in user
+        return obj.owner == request.user
+
+
+class IsSupporterOrReadOnly(permissions.BasePermission):
+    
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.supporter == request.user
